b5edd03636b98dc1656a63afa84b49fa523555de
tho888 <105895875+tho888@users.noreply.github.com>
2024-03-26 11:29:41 +1300

    Initial commit

---
+++ draw_letters.js
@@ -0,0 +1,47 @@
+/* these are optional special variables which will change the system */
+var systemBackgroundColor = "#acf2e7";
+var systemLineColor = "#000090";
+var systemBoxColor = "#C73869";
+
+/* internal constants */
+const darkGreen  = "#26b29d";
+const lightGreen  = "#30dfc4";
+const strokeColor  = "#0a2d27";
+
+/*
+ * Draw the letter given the letterData
+ *
+ * Letters should always be drawn with the
+ * following bounding box guideline:
+ * from (0,0) to (100, 200)
+ */
+function drawLetter(letterData) {
+  // color/stroke setup
+  stroke(strokeColor);
+  strokeWeight(4);
+
+  // determine parameters for second circle
+  let size2 = letterData["size"];
+  let pos2x = 50  + letterData["offsetx"];
+  let pos2y = 150 + letterData["offsety"];
+
+  // draw two circles
+  fill(darkGreen);
+  ellipse(50, 150, 75, 75);
+  fill(lightGreen);
+  ellipse(pos2x, pos2y, size2, size2);
+}
+
+function interpolate_letter(percent, oldObj, newObj) {
+  let new_letter = {};
+  new_letter["size"]    = map(percent, 0, 100, oldObj["size"], newObj["size"]);
+  new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
+  new_letter["offsety"] = map(percent, 0, 100, oldObj["offsety"], newObj["offsety"]);
+  return new_letter;
+}
+
+var swapWords = [
+  "ABBAABBA",
+  "CAB?CAB?",
+  "BAAAAAAA"
+]
