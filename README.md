8296192017a89155e87fb8dc8abdcb5319c1655e
tho888 <105895875+tho888@users.noreply.github.com>
2024-03-27 16:16:08 +1300

    Formatting

--- README.md
+++ README.md
@@ -8,3 +8,48 @@ The three parameters per letter:
   * `size` : radius of the second circle
   * `offsetx` : x offset of the second circle relative to the first one
   * `offsety` : y offset of the second circle relative to the first one
+
+___
+
+### PROGRESS DOCUMENTATION
+
+##### 25/03/24 - 31/03/24 *(Week 5)*
+
+- Started working on `sketch.js`
+- Experimenting with different parameters to add stems to lowercase letters 'a' and 'b'
+
+[EOW summary here]
+
+___
+
+##### 01/04/24 - 07/04/24 *(Week 6)*
+
+- 
+
+[EOW summary here]
+
+___
+
+##### 15/04/24 - 21/04/24 *(Week 7)*
+
+- 
+
+[EOW summary here]
+
+___
+
+##### 22/04/24 - 28/04/24 *(Week 8)*
+
+- 
+
+[EOW summary here]
+
+___
+
+##### 29/04/24 - 02/05/24 *(Week 9)*
+
+- 
+
+[EOW summary here]
+
+___
--- sketch.js
+++ sketch.js
@@ -17,10 +17,8 @@ const letterA = {
   "offsetx": 0,
   "offsety": 0,
   "lerpColorAmt": 0.3,
-  "lineLeftLower": 0,
-  "lineLeftUpper": 0,
-  "lineRightLower": 0,
-  "lineRightUpper": 55
+  "stemRightDescender": 0,
+  "stemRightAscender": 55
 }
 
 const letterB = {
@@ -28,17 +26,15 @@ const letterB = {
   "offsetx": 0,
   "offsety": 0,
   "lerpColorAmt": 0.4,
-  "lineLeftLower": 0,
-  "lineLeftUpper": 150
+  "stemLeftDescender": 0,
+  "stemLeftAscender": 150
 }
 
 const letterC = {
   "size": 50,
   "offsetx": 0,
   "offsety": 0,
-  "lerpColorAmt": 0.5,
-  "lineLeftLower": 0,
-  "lineLeftUpper": 0
+  "lerpColorAmt": 0.5
 }
 
 const darkGrey = "#241F1D";
@@ -78,11 +74,12 @@ function drawLetter(posx, posy, letterData) {
   let pos2y = posy + letterData["offsety"];
 
   //line variables
+  let stem1 = letterData["stemLeftDescender"];
+  let stem2 = letterData["stemLeftAscender"];
+  let stem3 = letterData["stemRightDescender"];
+  let stem4 = letterData["stemRightAscender"];
+
   let colorMixVar = letterData["lerpColorAmt"];
-  let lineLeftY1 = letterData["lineLeftLower"];
-  let lineLeftY2 = letterData["lineLeftUpper"];
-  let lineRightY1 = letterData["lineRightLower"];
-  let lineRightY2 = letterData["lineRightUpper"];
 
   let salmonCopy = color("#F2755E");
   let darkGreyCopy = color("#241F1D");
@@ -97,8 +94,8 @@ function drawLetter(posx, posy, letterData) {
   ellipse(pos2x, pos2y, size2, size2);
   pop();
 
-  line(posx - 50, posy + lineLeftY1, posx - 50, posy - lineLeftY2);
-  line(posx + 50, posy + lineRightY1, posx + 50, posy - lineRightY2);
+  line(posx - 50, posy + stem1, posx - 50, posy - stem2);
+  line(posx + 50, posy + stem3, posx + 50, posy - stem4);
 
 }
 
