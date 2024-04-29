70f874f0ec3a3c2a66248368c5e4762006138c44
tho888 <105895875+tho888@users.noreply.github.com>
2024-04-29 21:54:07 +1200

    Created typeface setup parameters
    - size/scale
    - strokeWeight
    - strokeCap

--- draw_letters.js
+++ draw_letters.js
@@ -8,39 +8,44 @@ const darkGrey = "#241F1D";
 const salmon = "#F2755E";
 const darkCyan = "#0D8AA1";
 
+// typeface setup parameters (acceptable values)
+const typefaceSize = 0.8; // set the scale (<=1)
+const typefaceStrokeWeight = 13; // set the strokeWeight (5-15)
+const typefaceStrokeCap = "ROUND"; // set the strokeCap ("ROUND" / "PROJECT")
+
 function drawLetter(letterData) {
 
   push();
 
   // stroke setup
-  strokeWeight(7);
-  strokeCap(ROUND); // 'ROUND' or 'PROJECT' works best
+  strokeWeight(typefaceStrokeWeight);
+  strokeCap(typefaceStrokeCap);
 
   // diagonal line parameters
-  let dLine1XPos = letterData["dLine1XPos"];
-  let dLine1YPos = letterData["dLine1YPos"];
-  let dLine1Length = letterData["dLine1Length"];
-  let dLine2XPos = letterData["dLine2XPos"];
-  let dLine2YPos = letterData["dLine2YPos"];
-  let dLine2Length = letterData["dLine2Length"];
+  let dLine1XPos = typefaceSize * letterData["dLine1XPos"];
+  let dLine1YPos = typefaceSize * letterData["dLine1YPos"];
+  let dLine1Length = typefaceSize * letterData["dLine1Length"];
+  let dLine2XPos = typefaceSize * letterData["dLine2XPos"];
+  let dLine2YPos = typefaceSize * letterData["dLine2YPos"];
+  let dLine2Length = typefaceSize * letterData["dLine2Length"];
 
   // horizontal line parameters
-  let hLine1YPos = letterData["hLine1YPos"];
-  let hLine1Length = letterData["hLine1Length"];
-  let hLine2YPos = letterData["hLine2YPos"];
-  let hLine2Length = letterData["hLine2Length"];
-  let hLine2Shift = letterData["hLine2Shift"];
-  let hLine3YPos = letterData["hLine3YPos"];
-  let hLine3Length = letterData["hLine3Length"];
+  let hLine1YPos = typefaceSize * letterData["hLine1YPos"];
+  let hLine1Length = typefaceSize * letterData["hLine1Length"];
+  let hLine2YPos = typefaceSize * letterData["hLine2YPos"];
+  let hLine2Length = typefaceSize * letterData["hLine2Length"];
+  let hLine2Shift = typefaceSize * letterData["hLine2Shift"];
+  let hLine3YPos = typefaceSize * letterData["hLine3YPos"];
+  let hLine3Length = typefaceSize * letterData["hLine3Length"];
 
   // vertical line parameters
-  let vLine1XPos = letterData["vLine1XPos"];
-  let vLine1Length = letterData["vLine1Length"];
-  let vLine2XPos = letterData["vLine2XPos"];
-  let vLine2Length = letterData["vLine2Length"];
-  let vLine2Shift = letterData["vLine2Shift"];
-  let vLine3Length = letterData["vLine3Length"];
-  let vLine3Shift = letterData["vLine3Shift"];
+  let vLine1XPos = typefaceSize * letterData["vLine1XPos"];
+  let vLine1Length = typefaceSize * letterData["vLine1Length"];
+  let vLine2XPos = typefaceSize * letterData["vLine2XPos"];
+  let vLine2Length = typefaceSize * letterData["vLine2Length"];
+  let vLine2Shift = typefaceSize * letterData["vLine2Shift"];
+  let vLine3Length = typefaceSize * letterData["vLine3Length"];
+  let vLine3Shift = typefaceSize * letterData["vLine3Shift"];
 
   // draw diagonal lines
   stroke(darkCyan);
@@ -57,13 +62,19 @@ function drawLetter(letterData) {
   stroke(darkGrey);
   line(vLine1XPos, 0, vLine1XPos, vLine1Length);
   line(vLine2XPos, vLine2Shift, vLine2XPos, vLine2Length + vLine2Shift);
-  line(50, 50 + vLine3Shift, 50, 50 + vLine3Length + vLine3Shift);
+  line(typefaceSize * 50, typefaceSize * 50 + vLine3Shift, typefaceSize * 50, typefaceSize * 50 + vLine3Length + vLine3Shift);
 
   pop();
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
+  new_letter["dLine1XPos"] = map(percent, 0, 100, oldObj["dLine1XPos"], newObj["dLine1XPos"]);
+  new_letter["dLine1YPos"] = map(percent, 0, 100, oldObj["dLine1YPos"], newObj["dLine1YPos"]);
+  new_letter["dLine1Length"] = map(percent, 0, 100, oldObj["dLine1Length"], newObj["dLine1Length"]);
+  new_letter["dLine2XPos"] = map(percent, 0, 100, oldObj["dLine2XPos"], newObj["dLine2XPos"]);
+  new_letter["dLine2YPos"] = map(percent, 0, 100, oldObj["dLine2YPos"], newObj["dLine2YPos"]);
+  new_letter["dLine2Length"] = map(percent, 0, 100, oldObj["dLine2Length"], newObj["dLine2Length"]);
   new_letter["hLine1YPos"] = map(percent, 0, 100, oldObj["hLine1YPos"], newObj["hLine1YPos"]);
   new_letter["hLine1Length"] = map(percent, 0, 100, oldObj["hLine1Length"], newObj["hLine1Length"]);
   new_letter["hLine2YPos"] = map(percent, 0, 100, oldObj["hLine2YPos"], newObj["hLine2YPos"]);
@@ -78,17 +89,11 @@ function interpolate_letter(percent, oldObj, newObj) {
   new_letter["vLine2Shift"] = map(percent, 0, 100, oldObj["vLine2Shift"], newObj["vLine2Shift"]);
   new_letter["vLine3Length"] = map(percent, 0, 100, oldObj["vLine3Length"], newObj["vLine3Length"]);
   new_letter["vLine3Shift"] = map(percent, 0, 100, oldObj["vLine3Shift"], newObj["vLine3Shift"]);
-  new_letter["dLine1XPos"] = map(percent, 0, 100, oldObj["dLine1XPos"], newObj["dLine1XPos"]);
-  new_letter["dLine1YPos"] = map(percent, 0, 100, oldObj["dLine1YPos"], newObj["dLine1YPos"]);
-  new_letter["dLine1Length"] = map(percent, 0, 100, oldObj["dLine1Length"], newObj["dLine1Length"]);
-  new_letter["dLine2XPos"] = map(percent, 0, 100, oldObj["dLine2XPos"], newObj["dLine2XPos"]);
-  new_letter["dLine2YPos"] = map(percent, 0, 100, oldObj["dLine2YPos"], newObj["dLine2YPos"]);
-  new_letter["dLine2Length"] = map(percent, 0, 100, oldObj["dLine2Length"], newObj["dLine2Length"]);
   return new_letter;
 }
 
 var swapWords = [
-  "WAVEFORM",
-  "BRAZENLY",
-  "HEXAPODS"
+  "GRIDLOCK",
+  "BLOCKAGE",
+  "SQR67890"
 ]
